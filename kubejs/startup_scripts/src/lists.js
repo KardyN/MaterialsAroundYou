b432270c80d92094660d1c53c17287558fbe840e
KardyN <zabolotnyinik@gmail.com>
2025-01-18 20:23:42 +0300

    global variables definition

---
+++ kubejs/startup_scripts/src/lists.js
@@ -0,0 +1,221 @@
+//priority: 1000
+global.oreTypes = [
+  {
+    id: "native_copper",
+    color: "#d88854",
+    type: "tfc_ore",
+    sand: "pink",
+  },
+  {
+    id: "native_gold",
+    color: "#c89d28",
+    type: "tfc_ore",
+    sand: "yellow",
+  },
+  {
+    id: "hematite",
+    color: "#b05e56",
+    type: "tfc_ore",
+    sand: "pink",
+  },
+  {
+    id: "native_silver",
+    color: "#b4b7c4",
+    type: "tfc_ore",
+    sand: "white",
+  },
+  {
+    id: "cassiterite",
+    color: "#6a6655",
+    type: "tfc_ore",
+    sand: "green",
+  },
+  {
+    id: "bismuthinite",
+    color: "#416f3d",
+    type: "tfc_ore",
+    sand: "green",
+  },
+  {
+    id: "garnierite",
+    color: "#3c5035",
+    type: "tfc_ore",
+    sand: "green",
+  },
+  {
+    id: "malachite",
+    color: "#587e79",
+    type: "tfc_ore",
+    sand: "green",
+  },
+  {
+    id: "magnetite",
+    color: "#3b3c3c",
+    type: "tfc_ore",
+    sand: "black",
+  },
+  {
+    id: "limonite",
+    color: "#93714a",
+    type: "tfc_ore",
+    sand: "yellow",
+  },
+  {
+    id: "sphalerite",
+    color: "#a4a3a3",
+    type: "tfc_ore",
+    sand: "white",
+  },
+  {
+    id: "tetrahedrite",
+    color: "#838283",
+    type: "tfc_ore",
+    sand: "black",
+  },
+  {
+    id: "gypsum",
+    color: "#977c62",
+    type: "tfc_mineral",
+    sand: "brown",
+  },
+  {
+    id: "graphite",
+    color: "#3a4249",
+    type: "tfc_mineral",
+    sand: "black",
+  },
+  {
+    id: "sulfur",
+    color: "#aaa552",
+    type: "tfc_mineral",
+    sand: "yellow",
+  },
+  {
+    id: "cinnabar",
+    color: "#ac161d",
+    type: "tfc_mineral",
+    sand: "red",
+  },
+  {
+    id: "cryolite",
+    color: "#909191",
+    type: "tfc_mineral",
+    sand: "white",
+  },
+  {
+    id: "saltpeter",
+    color: "#aaaaa9",
+    type: "tfc_mineral",
+    sand: "white",
+  },
+  {
+    id: "sylvite",
+    color: "#b98761",
+    type: "tfc_mineral",
+    sand: "yellow",
+  },
+  {
+    id: "borax",
+    color: "#a7a8a7",
+    type: "tfc_mineral",
+    sand: "white",
+  },
+  {
+    id: "bauxite",
+    color: "#e77263",
+    type: "tfc_ie_ore",
+    sand: "pink",
+  },
+  {
+    id: "galena",
+    color: "#5f5764",
+    type: "tfc_ie_ore",
+    sand: "black",
+  },
+  {
+    id: "uraninite",
+    color: "#7a9a65",
+    type: "tfc_ie_ore",
+    sand: "green",
+  },
+  {
+    id: "chromite",
+    color: "#ebebe8",
+    type: "firmalife_ore",
+    sand: "white",
+  },
+  //black, brown, green, pink, red, white, yellow
+  //amethyst, diamond, emerald, lapis_lazuli, opal, pyrite, ruby, sapphire, topaz "gem"
+];
+
+global.sandTypes = [
+  { id: "black", color: "#383838" },
+  { id: "brown", color: "#927755" },
+  { id: "green", color: "#626c45" },
+  { id: "pink", color: "#96706d" },
+  { id: "red", color: "#93634f" },
+  { id: "white", color: "#c9c9c9" },
+  { id: "yellow", color: "#dacea2" },
+];
+
+global.powderTypes = [
+  { id: "chromite", color: "#ebebe8" },
+  { id: "gypsum", color: "#977c62" },
+  { id: "cryolite", color: "#909191" },
+];
+
+global.metalToolHeads = [
+  "pickaxe_head",
+  "sword_blade",
+  "axe_head",
+  "shovel_head",
+  "hoe_head",
+  "chisel_head",
+  "hammer_head",
+  "saw_blade",
+  "knife_blade",
+  "scythe_blade",
+  "javelin_head",
+  "propick_head",
+  "mace_head",
+  "fish_hook",
+];
+
+global.osmiriteToolList = [
+  "pickaxe",
+  "sword",
+  "axe",
+  "shovel",
+  "hoe",
+  "chisel",
+  "hammer",
+  "saw",
+  "knife",
+  "scythe",
+  "javelin",
+  "propick",
+  "mace",
+];
+global.commonToolTagList = [
+  "immersiveengineering:toolbox/tools",
+  "minecraft:breaks_decorated_pots",
+  "minecraft:tools",
+  "forge:tools",
+  "tfc:metal_item/osmirite_tools",
+  "tfc:metal_item/osmirite",
+  "tfc:usable_on_tool_rack",
+];
+global.fishingRodTagList = [
+  "tfc:holds_small_fishing_bait",
+  "tfc:holds_large_fishing_bait",
+  "tfc:usable_on_tool_rack",
+  "tfc:metal_item/osmirite",
+  "forge:fishing_rods",
+  "forge:fishing_rods/osmirite",
+];
+global.shearsTagList = [
+  "tfc:usable_on_tool_rack",
+  "tfc:metal_item/osmirite",
+  "forge:shears",
+  "tfc:shears",
+];
