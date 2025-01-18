ae95b7a575020d9e58e8972e50c93c7534e8f3e8
KardyN <zabolotnyinik@gmail.com>
2025-01-18 21:26:09 +0300

    thick texture

--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,9 +1,4 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
-  event
-    .create("metal/osmirite")
-    .thickTexture("black")
-    .bucketColor("black")
-    .stillTexture("tfc:block/molten_still")
-    .flowingTexture("tfc:block/molten_flow");
+  event.create("metal/osmirite").thickTexture("#444444").bucketColor("#444444");
 });
