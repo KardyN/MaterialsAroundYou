02f96a62a6596953412c8857b27c01759f99e7e6
KardyN <zabolotnyinik@gmail.com>
2025-01-18 20:24:36 +0300

    uses global var

--- kubejs/server_scripts/src/recipes/tfc/grinding/dirtyDusts.js
+++ kubejs/server_scripts/src/recipes/tfc/grinding/dirtyDusts.js
@@ -1,5 +1,5 @@
 ServerEvents.recipes((event) => {
-  oreTypes.forEach((item) => {
+  global.oreTypes.forEach((item) => {
     event.recipes.tfc.quern(
       "1x kubejs:dirty_dust_" + item.id,
       "kubejs:clump_" + item.id
