a9183aa3ff3875b347d0bbae97052a48c9e2fa5e
KardyN <zabolotnyinik@gmail.com>
2025-01-19 16:56:58 +0300

    improved code

--- kubejs/server_scripts/src/items/multitool.js
+++ kubejs/server_scripts/src/items/multitool.js
@@ -1,27 +1,20 @@
-const swapToNext = (event) => {
-  let thisDamageValue = event.item.damageValue;
-  let thisId = event.item.id;
-  let nextId = global.multitoolIds[global.multitoolIds.indexOf(thisId) + 1];
-  event.player.setMainHandItem(Item.of(nextId, `{Damage:${thisDamageValue}}`));
-  event.player.cooldowns.addCooldown(nextId, 15);
+const swapToNext = (item, player) => {
+  let nextId = global.multitoolIds[global.multitoolIds.indexOf(item.id) + 1];
+  player.setMainHandItem(Item.of(nextId, `{Damage:${item.damageValue}}`));
+  player.cooldowns.addCooldown(nextId, 15);
 };
 
 BlockEvents.rightClicked(/kubejs:metal\/.*prospector.*\/osmirite/, (event) => {
-  if (
-    event.player.crouching &&
-    !event.player.cooldowns.isOnCooldown(event.player.mainHandItem)
-  ) {
-    swapToNext(event);
+  const { item, player } = event;
+  if (player.crouching && !player.cooldowns.isOnCooldown(player.mainHandItem)) {
+    swapToNext(item, player);
     event.cancel();
   }
 });
 
 ItemEvents.rightClicked(/kubejs:metal\/.*prospector.*\/osmirite/, (event) => {
-  if (
-    event.player.crouching == true &&
-    !event.player.cooldowns.isOnCooldown(event.player.mainHandItem)
-  ) {
-    swapToNext(event);
+  const { item, player } = event;
+  if (player.crouching && !player.cooldowns.isOnCooldown(player.mainHandItem)) {
+    swapToNext(item, player);
   }
-  event.player.setMainHandItem();
 });
