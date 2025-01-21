45598675afaf0f7b60f4eb098ae274192d1dd384
KardyN <zabolotnyinik@gmail.com>
2025-01-22 02:09:18 +0300

    Update ore_small.js

--- kubejs/server_scripts/src/recipes/tfc/crafting/ore_small.js
+++ kubejs/server_scripts/src/recipes/tfc/crafting/ore_small.js
@@ -1,67 +1,14 @@
 ServerEvents.recipes((event) => {
   global.oreList.forEach((ore) => {
-    switch (ore.mod) {
-      case "tfc":
+    Object.entries(global.smallOreYieldByQualityDictionary).forEach(
+      ([quality, amount]) => {
         event
-          .shapeless("2x tfc:ore/small_" + ore.name, [
-            "tfc:ore/poor_" + ore.name,
+          .shapeless(`${amount}x ${ore.mod}:ore/small_${ore.name}`, [
+            `${ore.mod}:ore/${quality}_${ore.name}`,
             "#tfc:hammers",
           ])
           .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("3x tfc:ore/small_" + ore.name, [
-            "tfc:ore/normal_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("5x tfc:ore/small_" + ore.name, [
-            "tfc:ore/rich_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-
-        break;
-      case "tfc_ie_addon":
-        event
-          .shapeless("2x tfc_ie_addon:ore/small_" + ore.name, [
-            "tfc_ie_addon:ore/poor_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("3x tfc_ie_addon:ore/small_" + ore.name, [
-            "tfc_ie_addon:ore/normal_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("5x tfc_ie_addon:ore/small_" + ore.name, [
-            "tfc_ie_addon:ore/rich_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        break;
-      case "firmalife":
-        event
-          .shapeless("2x firmalife:ore/small_" + ore.name, [
-            "firmalife:ore/poor_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("3x firmalife:ore/small_" + ore.name, [
-            "firmalife:ore/normal_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        event
-          .shapeless("5x firmalife:ore/small_" + ore.name, [
-            "firmalife:ore/rich_" + ore.name,
-            "#tfc:hammers",
-          ])
-          .damageIngredient("#tfc:hammers", 1);
-        break;
-    }
+      }
+    );
   });
 });
