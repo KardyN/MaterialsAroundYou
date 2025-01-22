939c3f401606452ba65f3bbed9b9705d63c222d7
KardyN <zabolotnyinik@gmail.com>
2025-01-23 02:09:38 +0300

    added bearing alloy fluid

--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,6 +1,7 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
   event.create("metal/osmirite").thickTexture("#444444");
+  event.create("metal/bearing_alloy").thickTexture("#444444");
   event.create("ore/cassiterite").thickTexture("#444444");
   event.create("ore/hematite").thickTexture("#444444");
   event.create("ore/magnetite").thickTexture("#444444");
