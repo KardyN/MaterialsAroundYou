0b9d24425bc9fbe3ae09ee46dfc4e4c842f08cf8
KardyN <zabolotnyinik@gmail.com>
2025-01-22 17:58:45 +0300

    added some fluid ores

--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,8 +1,8 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
   event.create("metal/osmirite").thickTexture("#444444").bucketColor("#444444");
-  event
-    .create("ore/cassiterite")
-    .thickTexture("#888899")
-    .bucketColor("#888899");
+  event.create("ore/cassiterite");
+  event.create("ore/hematite");
+  event.create("ore/magnetite");
+  event.create("ore/limonite");
 });
