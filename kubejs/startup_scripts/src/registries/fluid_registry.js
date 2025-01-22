72228dd37f8c519f60ca42b44e3fbf16bdaea1c9
KardyN <zabolotnyinik@gmail.com>
2025-01-22 21:53:30 +0300

    Update fluid_registry.js

--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,8 +1,8 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
-  event.create("metal/osmirite").thickTexture("#444444").bucketColor("#444444");
-  event.create("ore/cassiterite");
-  event.create("ore/hematite");
-  event.create("ore/magnetite");
-  event.create("ore/limonite");
+  event.create("metal/osmirite").thickTexture("#444444");
+  event.create("ore/cassiterite").thickTexture("#444444");
+  event.create("ore/hematite").thickTexture("#444444");
+  event.create("ore/magnetite").thickTexture("#444444");
+  event.create("ore/limonite").thickTexture("#444444");
 });
