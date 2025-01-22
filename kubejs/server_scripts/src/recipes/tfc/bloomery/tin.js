15eb4509c686ae9131e8eb61e52fda91ef9ca754
KardyN <zabolotnyinik@gmail.com>
2025-01-22 17:58:28 +0300

    Update tin.js

--- kubejs/server_scripts/src/recipes/tfc/bloomery/tin.js
+++ kubejs/server_scripts/src/recipes/tfc/bloomery/tin.js
@@ -2,7 +2,7 @@ ServerEvents.recipes((event) => {
   event.custom({
     type: "tfc:bloomery",
     result: { item: "tfc:metal/ingot/tin" },
-    fluid: { ingredient: "kubejs:metal/cassiterite", amount: 100 },
+    fluid: { ingredient: "kubejs:ore/cassiterite", amount: 100 },
     catalyst: {
       ingredient: { item: "tfc:powder/charcoal" },
       count: 3,
