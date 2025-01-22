bc60e551dff58e5dd183c049c0cb1f5f30244a4c
KardyN <zabolotnyinik@gmail.com>
2025-01-22 17:31:04 +0300

    cassiterite refinement

---
+++ kubejs/server_scripts/src/recipes/tfc/bloomery/cassiterite.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes((event) => {
+  event.custom({
+    type: "tfc:bloomery",
+    result: { item: "tfc:metal/ingot/tin" },
+    fluid: { ingredient: "kubejs:metal/cassiterite", amount: 100 },
+    catalyst: {
+      ingredient: { item: "tfc:powder/charcoal" },
+      count: 3,
+    },
+    duration: 15000,
+  });
+});
--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,4 +1,8 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
   event.create("metal/osmirite").thickTexture("#444444").bucketColor("#444444");
+  event
+    .create("ore/cassiterite")
+    .thickTexture("#888899")
+    .bucketColor("#888899");
 });
