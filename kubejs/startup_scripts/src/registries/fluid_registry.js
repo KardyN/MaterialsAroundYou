73412479b4bd598cfed31772a07d88dc4514e174
KardyN <zabolotnyinik@gmail.com>
2025-01-23 16:14:26 +0300

    removed buckets and placeablity

--- kubejs/startup_scripts/src/registries/fluid_registry.js
+++ kubejs/startup_scripts/src/registries/fluid_registry.js
@@ -1,9 +1,15 @@
 //priority: 99
 StartupEvents.registry("fluid", (event) => {
-  event.create("metal/osmirite").thickTexture("#444444");
-  event.create("metal/bearing_alloy").thickTexture("#444444");
-  event.create("ore/cassiterite").thickTexture("#444444");
-  event.create("ore/hematite").thickTexture("#444444");
-  event.create("ore/magnetite").thickTexture("#444444");
-  event.create("ore/limonite").thickTexture("#444444");
+  //metals
+  event.create("metal/osmirite").thickTexture("#444444").noBlock().noBucket();
+  event
+    .create("metal/bearing_alloy")
+    .thickTexture("#444444")
+    .noBlock()
+    .noBucket();
+  //molten ores
+  event.create("ore/cassiterite").thickTexture("#444444").noBlock().noBucket();
+  event.create("ore/hematite").thickTexture("#444444").noBlock().noBucket();
+  event.create("ore/magnetite").thickTexture("#444444").noBlock().noBucket();
+  event.create("ore/limonite").thickTexture("#444444").noBlock().noBucket();
 });
