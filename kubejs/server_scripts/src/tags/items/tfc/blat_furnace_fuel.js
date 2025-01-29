d632fe8721205095e4df15f76ca535e80fbef219
KardyN <zabolotnyinik@gmail.com>
2025-01-30 02:25:58 +0300

    updated tags

--- kubejs/server_scripts/src/tags/items/tfc/blat_furnace_fuel.js
+++ kubejs/server_scripts/src/tags/items/tfc/blat_furnace_fuel.js
@@ -3,4 +3,8 @@ ServerEvents.tags("item", (event) => {
   event.add("tfc:blast_furnace_fuel", "immersiveengineering:coal_coke");
 
   event.add("tfc:blooms", ["kubejs:raw_tin_bloom", "kubejs:refined_tin_bloom"]);
+
+  event.remove("tfc:axles", /tfc:wood\/encased_axle\/.+/);
+  event.add("tfc:encased_axles", /tfc:wood\/encased_axle\/.+/);
+  event.add("tfc:lumber", /.+treated.+lumber/);
 });
