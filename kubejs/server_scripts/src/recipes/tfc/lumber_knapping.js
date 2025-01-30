99caeef093bfbde7bdc8d7fe068764a5701938e6
KardyN <zabolotnyinik@gmail.com>
2025-01-30 19:51:05 +0300

    Update lumber_knapping.js

--- kubejs/server_scripts/src/recipes/tfc/lumber_knapping.js
+++ kubejs/server_scripts/src/recipes/tfc/lumber_knapping.js
@@ -1,19 +1,16 @@
 ServerEvents.recipes((event) => {
   let recipes = [
     {
-      knapping_type: "tfc:lumber",
       pattern: ["     ", "  X  ", " XXX ", "  X  ", "     "],
       result: { item: "kubejs:wooden_gear", count: 1 },
       id: "tfc/lumber_knapping/wooden_gear_1",
     },
     {
-      knapping_type: "tfc:lumber",
       pattern: ["   X ", "  XXX", " X X ", "XXX  ", " X   "],
       result: { item: "kubejs:wooden_gear", count: 2 },
       id: "tfc/lumber_knapping/wooden_gear_2",
     },
     {
-      knapping_type: "tfc:lumber",
       pattern: ["X X X", " XXX ", "XX XX", " XXX ", "X X X"],
       result: { item: "kubejs:large_wooden_gear", count: 1 },
       id: "tfc/lumber_knapping/large_wooden_gear",
@@ -22,6 +19,7 @@ ServerEvents.recipes((event) => {
 
   recipes.forEach((recipe) => {
     recipe.type = "tfc:knapping";
+    recipe.knapping_type = "tfc:lumber";
     event.custom(recipe).id(recipe.id);
   });
 });
