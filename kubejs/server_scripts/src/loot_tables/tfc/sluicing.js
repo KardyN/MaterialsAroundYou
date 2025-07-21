15ef50bc9137397d052f7052ca1fcf61250a422f
KardyN <zabolotnyinik@gmail.com>
2025-07-21 10:36:47 +0300

    optimised code

--- kubejs/server_scripts/src/loot_tables/tfc/sluicing.js
+++ kubejs/server_scripts/src/loot_tables/tfc/sluicing.js
@@ -1,58 +1,54 @@
-const wrapperAddJson = (event, ore) => {
-  const { name, mod, sand, gems } = ore;
-  let newJson = {
-    type: "minecraft:empty",
-    pools: [
-      {
-        name: "loot_pool",
-        rolls: 1,
-        entries: [
-          {
-            type: "minecraft:alternatives",
-            children: [
-              {
-                type: "minecraft:item",
-                name:
-                  name == "chromite"
-                    ? `kubejs:powder/${name}`
-                    : `${mod}:powder/${name}`,
-                conditions: [
-                  {
-                    condition: "minecraft:random_chance",
-                    chance: 0.999,
-                  },
-                ],
-              },
-              {
-                type: "minecraft:group",
-                children: [],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        rolls: 1,
-        entries: [
-          {
-            type: "minecraft:item",
-            name: `kubejs:pile/${sand}_sand`,
-          },
-        ],
-      },
-    ],
-  };
-  gems.forEach((gem) => {
-    newJson.pools[0].entries[0].children[1].children.push({
-      type: "minecraft:item",
-      name: `tfc:powder/${gem}`,
-    });
-  });
-  event.addJson(`kubejs:dirty_dust/${name}`, newJson);
-};
-
 ServerEvents.genericLootTables((event) => {
   global.oreList.forEach((ore) => {
-    wrapperAddJson(event, ore);
+    let { name, mod, sand, gems } = ore;
+    let newJson = {
+      type: "minecraft:empty",
+      pools: [
+        {
+          name: "loot_pool",
+          rolls: 1,
+          entries: [
+            {
+              type: "minecraft:alternatives",
+              children: [
+                {
+                  type: "minecraft:item",
+                  name:
+                    name == "chromite"
+                      ? `kubejs:powder/${name}`
+                      : `${mod}:powder/${name}`,
+                  conditions: [
+                    {
+                      condition: "minecraft:random_chance",
+                      chance: 0.999,
+                    },
+                  ],
+                },
+                {
+                  type: "minecraft:group",
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          rolls: 1,
+          entries: [
+            {
+              type: "minecraft:item",
+              name: `kubejs:pile/${sand}_sand`,
+            },
+          ],
+        },
+      ],
+    };
+    gems.forEach((gem) => {
+      newJson.pools[0].entries[0].children[1].children.push({
+        type: "minecraft:item",
+        name: `tfc:powder/${gem}`,
+      });
+    });
+    event.addJson(`kubejs:sluicing/dirty_dust/${name}`, newJson);
   });
 });
