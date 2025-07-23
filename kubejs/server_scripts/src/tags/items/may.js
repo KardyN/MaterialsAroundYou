6e80b349d367bb52b48e61ebd46f4c177866e02a
KardyN <zabolotnyinik@gmail.com>
2025-07-23 23:49:33 +0300

    revamped tags folder structure

--- kubejs/server_scripts/src/tags/items/may.js
+++ kubejs/server_scripts/src/tags/items/may.js
@@ -1,12 +1,7 @@
 ServerEvents.tags("item", (event) => {
-  event.add("forge:dusts", /kubejs:powder.+/);
-  event.add("tfc:powders", /kubejs:powder.+/);
   event.add("may:dirty_dusts", /kubejs:dirty_dust.+/);
   event.add("may:piles", /^kubejs:pile((?!sand).)*$/);
   event.add("may:briquettes", /kubejs:briquette.+/);
   event.add("may:heap/sand", /kubejs:pile\/.+sand/);
   event.add("may:blooms", /kubejs:raw.+bloom/);
-  global.metalToolHeads.forEach((tool) => {
-    event.add("tfc:metal_item/osmirite", `kubejs:metal/${tool}/osmirite`);
-  });
 });
--- kubejs/server_scripts/src/tags/items/tfc.js
+++ kubejs/server_scripts/src/tags/items/tfc.js
@@ -1,9 +1,13 @@
 ServerEvents.tags("item", (event) => {
   event.removeAll("tfc:blast_furnace_fuel");
   event.add("tfc:blast_furnace_fuel", "immersiveengineering:coal_coke");
-
+  event.add("tfc:powders", /kubejs:powder.+/);
+  global.metalToolHeads.forEach((tool) => {
+    event.add("tfc:metal_item/osmirite", `kubejs:metal/${tool}/osmirite`);
+  });
+  event.removeAllTagsFrom(/tfc:stone\/hammer\/.+/);
   event.add("tfc:blooms", ["kubejs:raw_tin_bloom", "kubejs:refined_tin_bloom"]);
-
+  event.remove("tfc:blooms", /kubejs:.+bloom/);
   event.remove("tfc:axles", /tfc:wood\/encased_axle\/.+/);
   event.add("tfc:encased_axles", /tfc:wood\/encased_axle\/.+/);
   event.add("tfc:lumber", /.+treated_wood_lumber/);
