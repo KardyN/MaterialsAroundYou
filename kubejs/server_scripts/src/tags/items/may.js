dbdd29dea05d7a3d94b027b990cdfca3f3eaad63
KardyN <zabolotnyinik@gmail.com>
2025-07-25 23:16:07 +0300

    Update may.js

--- kubejs/server_scripts/src/tags/items/may.js
+++ kubejs/server_scripts/src/tags/items/may.js
@@ -1,4 +1,7 @@
 ServerEvents.tags("item", (event) => {
+  global.powderList.forEach((powder) => {
+    event.add("may:ore_powders", `${powder.mod}:powder/${powder.name}`);
+  });
   event.add("may:dirty_dusts", /kubejs:dirty_dust.+/);
   event.add("may:piles", /^kubejs:pile((?!sand).)*$/);
   event.add("may:briquettes", /kubejs:briquette.+/);
