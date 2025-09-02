f8043a78dbd53c18b42a8f084f15e96bf82faf47
KardyN <zabolotnyinik@gmail.com>
2025-09-02 21:42:50 +0300

    Update mineral.js
    impl custom mineral vein builder + removal unwanted veins

--- kubejs/server_scripts/src/recipes/immersiveengineering/mineral.js
+++ kubejs/server_scripts/src/recipes/immersiveengineering/mineral.js
@@ -1,27 +1,39 @@
 ServerEvents.recipes((event) => {
-  let recipes = [
-    MineralMixRecipeBuilder()
-      .addId("kubejs:testrecipe")
-      .addOutput(0.5, "minecraft:diamond")
-      .addSpoil(0.5, "minecraft:stone"),
-  ];
+  let recipes = [];
 
   recipes.forEach((recipe) => {
     recipe.type = "immersiveengineering:mineral_mix";
     event.custom(recipe).id(recipe.id);
   });
+
+  let disabledMinerals = [
+    { id: "immersiveengineering:mineral/amethyst_crevasse" },
+    { id: "immersiveengineering:mineral/ancient_seabed" },
+    { id: "immersiveengineering:mineral/ancient_debris" },
+    { id: "immersiveengineering:mineral/cooled_lava_tube" },
+    { id: "immersiveengineering:mineral/hardened_clay_pan" },
+    { id: "immersiveengineering:mineral/igneous_rock" },
+    { id: "immersiveengineering:mineral/mephitic_quarzite" },
+    { id: "immersiveengineering:mineral/silt" },
+    { id: "immersiveengineering:mineral/nether_silt" },
+    { id: "createaddition:compat/immersiveengineering/sphalerite" },
+  ];
+
+  disabledMinerals.forEach((disabledRecipe) => {
+    event.remove(disabledRecipe);
+  });
 });
 
 function MineralMixRecipeBuilder() {
   let recipe = {
-    _id: "",
+    _id: "may:mineral/test_mineral",
     _ores: [],
     _spoils: [],
-    _weight: 1,
-    _fail_chance: 0,
+    _weight: 10,
+    _fail_chance: 0.1,
     _dimensions: ["minecraft:overworld"],
-    //_background: {},
 
+    // Recipe namespace
     addId: function (id) {
       this._id = id;
       return this;
@@ -29,43 +41,42 @@ function MineralMixRecipeBuilder() {
 
     addOutput: function (chance, output) {
       output[0] == "#"
-        ? this._ores.add({ chance: chance, output: { tag: output.slice(1) } })
-        : this._ores.add({ chance: chance, output: { item: output } });
+        ? this._ores.push({ chance: chance, output: { tag: output.slice(1) } })
+        : this._ores.push({ chance: chance, output: { item: output } });
       return this;
     },
 
     addSpoil: function (chance, spoil) {
-      output[0] == "#"
-        ? this._ores.add({ chance: chance, output: { tag: spoil.slice(1) } })
-        : this._ores.add({ chance: chance, output: { item: spoil } });
+      spoil[0] == "#"
+        ? this._spoils.push({ chance: chance, output: { tag: spoil.slice(1) } })
+        : this._spoils.push({ chance: chance, output: { item: spoil } });
       return this;
     },
 
+    // How rare the vein is // ~10 rare - ~30 common
     addWeight: function (weight) {
       this._weight = weight;
       return this;
     },
 
+    // how likely to yield bad product instead of ore // ~0.05 rich - ~0.15 poor
     addFailChance: function (failChance) {
       this._failChance = failChance;
       return this;
     },
 
+    // Dimensions to spawn the vein in // Default: overworld
     addDimensions: function (dimensions) {
       this._dimensions = dimensions;
       return this;
     },
 
-    // addBackground: function (id) {
-    //   this._id = id;
-    //   return this;
-    // },
     build: function () {
       return {
         id: this._id,
         ores: this._ores,
         spoils: this._spoils,
-        weight: this._id,
+        weight: this._weight,
         failChance: this._fail_chance,
         dimensions: this._dimensions,
       };
